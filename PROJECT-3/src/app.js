89c0a3d33638653ba0eedbc9ff5e2dd6d0692631
chintan-harkhani <harkhanichintan943@gmail.com>
2023-08-09 13:57:19 +0530

    ok

---
+++ PROJECT-3/src/app.js
@@ -0,0 +1,13 @@
+const express  =  require("express");
+const http = require("http");
+const {connectDB} =require("./db/dbconnection");
+const config = require("./config/config")
+const app = express();
+
+//databse connect
+connectDB();
+
+//simple server create
+http.createServer(app).listen(config.port,()=>{
+     console.log("Server SuccessFully Listing PORT Number in 3000 !...");
+});
